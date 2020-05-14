2f91161ee99ab4a46871432ff26f5944928d8b85
AaronKwong929 <aaronkwong929@gmail.com>
2020-05-14 16:06:45 +0800

    133

--- src/core/mergeConfig.ts
+++ src/core/mergeConfig.ts
@@ -1,35 +1,47 @@
 import { AxiosRequestConfig } from './../types/index';
 import { isPlainObject, deepMerge } from '../helpers/util';
 
+// 空对象：合并策略
 const starts = Object.create(null);
 
+// val2 可能出现的值
 const startKeysFromVal2 = ['url', 'params', 'data'];
 
+// 将 val2 的键全部加上 fromVal2Strat 的方法 => 键 url - 值 函数方法
+// url params data 参数，只要传入的 config 有，就要合并到配置里面
+// 没有就不管，用 fromVal2Strat
 startKeysFromVal2.forEach(key => {
     starts[key] = fromVal2Start;
 });
 
+// 默认的合并方法，当 strats 对象没有这个键的对应方法的时候就用默认的
+// val2 不为空就赋 val2 否则 val1
 function defaultStart(val1: any, val2: any): any {
     return typeof val2 !== 'undefined' ? val2 : val1;
 }
 
+// 只看 val2，有值就赋
 function fromVal2Start(val1: any, val2: any): any {
     if (typeof val2 !== 'undefined') return val2;
 }
 
 function deepMergeStart(val1: any, val2: any): any {
-    // val2 可能是个字符串 可能不是对象
     if (isPlainObject(val2)) {
+        // val2 是对象
         return deepMerge(val1, val2);
     } else if (typeof val2 !== 'undefined') {
+        // val2 不是对象 且 val2 不为空
         return val2;
     } else if (isPlainObject(val1)) {
+        // val2 不是对象，且 val2 为空，且 val1 为对象
         return deepMerge(val1);
     } else if (typeof val1 !== 'undefined') {
+        // val2 不是对象， 且 val2 为空，且 val1 不是对象
         return val1;
     }
 }
 
+// headers需要深克隆
 const startKeysDeepMerge = ['headers'];
 
 startKeysDeepMerge.forEach(key => {
--- src/helpers/util.ts
+++ src/helpers/util.ts
@@ -18,19 +18,27 @@ export function extend<T, U>(to: T, from: U): T & U {
 
 // 深克隆
 export function deepMerge(...objs: Array<any>): any {
+    // 新建空对象
     const result = Object.create(null);
-
+    
     objs.forEach(obj => {
+        // 如果非空
         if (obj) {
             Object.keys(obj).forEach(key => {
+                // 每个键对应的值
                 const val = obj[key];
+                
                 if (isPlainObject(val)) {
+                    // 值是对象
                     if (isPlainObject(result[key])) {
+                        // 目标对象内该键的值是一个对象
                         result[key] = deepMerge(result[key], val);
                     } else {
+                        // 目标对象内没有该键
                         result[key] = deepMerge(val);
                     }
                 } else {
+                    // 值不是对象
                     result[key] = val;
                 }
             });
